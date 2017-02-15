0e2d0554b916230239c9f365d092b72fb4b06358
Juan Manuel Lopez <jlopezjuy@gmail.com>
2017-02-15 16:23:05 -0300

    Cambio de scope

--- Maven/ClothesService/ClothesWeb/src/main/java/ar/com/clothes/bean/ClienteBean.java
+++ Maven/ClothesService/ClothesWeb/src/main/java/ar/com/clothes/bean/ClienteBean.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -21,7 +21,7 @@ import ar.com.clothes.model.Empresa;
 import ar.com.clothes.util.StringUtil;
 
 @ManagedBean(name = "clienteBean")
-@ViewScoped
+@SessionScoped
 public class ClienteBean extends BaseBean {
 	private static final Logger LOG = Logger.getLogger(ClienteBean.class);
 
